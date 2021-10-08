1349a4bf46629c1e8b933840435ef02faed24f57
Santosh Prasad <santosh.prasad@halliburton.com>
2021-10-08 17:59:54 +0530

    web server done

--- main.tf
+++ main.tf
@@ -2,11 +2,27 @@ provider "aws" {
     region = "ap-south-1"
 }
 
-resource "aws_instance" "vm-1" {
-    ami = "ami-0967a8b99f328dfee"
+resource "aws_instance" "webserver" {
+    ami = "ami-00d3938d52d531b8e"
     instance_type = "t2.micro"
-
+    vpc_security_group_ids = [aws_security_group.webServerSG-1.id]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello World " > index.html
+                nohup busybox httpd -f -p "${var.server_port}" &
+                EOF
     tags = {
-        Name = "vm-1"
+        Name = "webserver-1"
     }
+}
+#By default AWS does not allow any in/out traffic from ec2
+#create a security group resource to make web server work
+resource "aws_security_group" "webServerSG-1" {
+  name = "webServerSG-1"
+  ingress {
+      from_port = var.server_port
+      to_port = var.server_port
+      protocol = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+  }
 }
